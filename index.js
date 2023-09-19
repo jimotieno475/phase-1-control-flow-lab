b06cc8ed12a6bf0e64f399fd2040e58240fb2cd0
jim <jim.otieno@student.moringaschool.com>
2023-09-19 11:01:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(t){
   // Write your code here!
+  if (t<=400) {
+    return('This one is on me!');
+  } else if(t>200 & t<=2500){
+    return('I will gladly take your thirty bucks.');
+
+  } else{
+    return('No can do.');
+  }
+  
+  }
+
+function ternaryCheckCity(p){
+  // Write your code here!
+  if (p==='NYC') {
+    return'Ok, sounds good.';
+  } else{
+    return'No go.';
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(a){
   // Write your code here!
-}
+  switch (a) {
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    default:
+      return 'Bye.';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    
+  }
+
 }
